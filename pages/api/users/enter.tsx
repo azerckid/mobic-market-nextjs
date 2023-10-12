795528a51ef9fade746da7284f44cc09a40c7e5a
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-10-12 16:53:22 +0900

    add phone & email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,11 @@
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -33,6 +38,25 @@ async function handler(
     },
   });
   console.log(token);
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+      body: `Your verification code is ${payload}`,
+      from: process.env.TWILIO_PHONE,
+      to: process.env.MY_PHONE!,
+    });
+    console.log(message);
+  } else if (email) {
+    const message = {
+      to: process.env.SENDGRID_FROM!,
+      from: process.env.SENDGRID_FROM!,
+      subject: "Your verification code",
+      text: `Your verification code is ${payload}`,
+      html: `<strong>Your mobic-market verification code is ${payload}</strong>`,
+    };
+    const emailResponse = await mail.send(message);
+    console.log(emailResponse);
+  }
   return res.status(200).json({
     ok: true,
     token: token.payload,
